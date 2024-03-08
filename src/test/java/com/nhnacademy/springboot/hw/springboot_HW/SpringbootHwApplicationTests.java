b4c9a2d0ea7aa0973815a76747557765f40e22b6
SeoYeonLee12 <134525851+SeoYeonLee12@users.noreply.github.com>
2024-03-08 13:41:39 +0900

    Initializer

---
+++ src/test/java/com/nhnacademy/springboot/hw/springboot_HW/SpringbootHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.springboot.hw.springboot_HW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
